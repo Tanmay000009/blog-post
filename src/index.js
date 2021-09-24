33e3ed58f79b9f13906b8af7a9a19754657f2636
Tanmay000009 <tanmayvyas09@gmail.com>
2021-09-24 20:50:17 +0530

    Wrapping child component under parent

--- src/index.js
+++ src/index.js
@@ -2,13 +2,20 @@ import React from "react";
 import ReactDom from "react-dom";
 import faker from "faker";
 import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail author="Tanmay" avatar={faker.image.avatar()} time="today at 1:00PM" comment="op" />
-            <CommentDetail author="Aman" avatar={faker.image.avatar()} time="today at 3:00PM" comment="god"/>
-            <CommentDetail author="Arjun" avatar={faker.image.avatar()} time="today at 5:00PM" comment="nice" />
+            <ApprovalCard >
+                <CommentDetail author="Tanmay" avatar={faker.image.avatar()} time="today at 1:00PM" comment="op" />
+            </ApprovalCard>
+            <ApprovalCard >
+                <CommentDetail author="Aman" avatar={faker.image.avatar()} time="today at 3:00PM" comment="god"/>
+            </ApprovalCard>
+            <ApprovalCard >
+                <CommentDetail author="Arjun" avatar={faker.image.avatar()} time="today at 5:00PM" comment="nice" />
+            </ApprovalCard>
         </div>
     );
 };
