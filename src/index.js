4da99119383188fba9d3e0e421ec1958ef135dfa
Tanmay000009 <tanmayvyas09@gmail.com>
2021-09-24 11:06:02 +0530

    Basic components setup

--- src/index.js
+++ src/index.js
@@ -2,7 +2,26 @@ import React from "react";
 import ReactDom from "react-dom";
 
 const App = () => {
-    return <div> Hi </div>
+    return (
+        <div className="ui container comments">
+            <div className="comment">
+                <a href="/" className="avatar">
+                    <img alt="avatar" />
+                </a>
+                <div className="content">
+                    <a href="/" className="author">
+                        Tanmay
+                    </a>
+                    <div className="metadata">
+                        <span className="date">Today at 6:00PM</span>
+                    </div>
+                    <div className="text">
+                        Nice blog post!
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 ReactDom.render(<App />,document.querySelector('#root'));
